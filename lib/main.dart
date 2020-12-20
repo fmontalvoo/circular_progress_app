44ff125bd4720efd5f02c2b3e22b23aa04701849
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2020-12-20 16:57:26 -0500

    Widget RadialProgressIndicator.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+// import 'package:circular_progress_app/pages/circular_progress_page.dart';
+import 'package:circular_progress_app/pages/home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Circular Progress App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(),
+    );
+  }
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,44 @@
+import 'package:circular_progress_app/widgets/radial_progress_indicator.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  double porcentaje = 0.0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+            width: 250.0,
+            height: 250.0,
+            child: RadialProgressIndicator(
+              percentage: porcentaje,
+              outStrokeColor: Colors.blue,
+              inStrokeColor: Colors.grey,
+              inStrokeWitdh: 5.0,
+              outStrokeWitdh: 10.0,
+              gradient: LinearGradient(colors: <Color>[
+                Color(0xffc012ff),
+                Color(0xff6d05ea),
+                Colors.red
+              ]),
+            )),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.refresh),
+        onPressed: _refresh,
+      ),
+    );
+  }
+
+  void _refresh() {
+    setState(() {
+      porcentaje += 10.0;
+      if (porcentaje > 100.0) porcentaje = 0.0;
+    });
+  }
+}
